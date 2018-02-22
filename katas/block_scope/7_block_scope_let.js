d4d8b7363d6eee352dee2cea76901226292feacf
Anthony Su <anthonysu@radialglo.com>
2018-02-22 14:39:24 -0800

    Finish 7 block scope - let. Let is block scoped

--- katas/block_scope/7_block_scope_let.js
+++ katas/block_scope/7_block_scope_let.js
@@ -8,14 +8,14 @@ describe('`let` restricts the scope of the variable to the current block', () =>
 
     it('`var` works as usual', () => {
       if (true) {
-        let varX = true;
+        var varX = true;
       }
       assert.equal(varX, true);
     });
     
     it('`let` restricts scope to inside the block', () => {
       if (true) {
-        var letX = true;
+        let letX = true;
       }
       assert.throws(() => console.log(letX));
     });
@@ -26,13 +26,14 @@ describe('`let` restricts the scope of the variable to the current block', () =>
     
     it('`let` use in `for` loops', () => {
       let obj = {x: 1};
-      for (var key in obj) {}
+      // key here is only scoped to for loop
+      for (let key in obj) {}
       assert.throws(() => console.log(key));
     });
     
     it('create artifical scope, using curly braces', () => {
       {
-        var letX = true;
+        let letX = true;
       }
       assert.throws(() => console.log(letX));
     });
