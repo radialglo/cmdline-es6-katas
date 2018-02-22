75c75bb24ce9b291f07f5c3cab8a0f0123ea95e2
Anthony  Su <antchovee@gmail.com>
2018-02-21 21:50:04 -0800

    Add in  23 - accessors (getters and setters) and 24 - static

--- katas/class/24_class_static.js
+++ katas/class/24_class_static.js
@@ -11,7 +11,7 @@ describe('inside a class you can use the `static` keyword', () => {
     
     it('a static method just has the prefix `static`', () => {
       class TestFactory {
-        makeTest() { return new UnitTest(); }
+        static makeTest() { return new UnitTest(); }
       }
       
       assert.ok(TestFactory.makeTest() instanceof UnitTest);
@@ -23,14 +23,14 @@ describe('inside a class you can use the `static` keyword', () => {
         static [methodName]() { return new UnitTest(); }
       }
       
-      assert.ok(TestFactory.createTest() instanceof UnitTest);
+      assert.ok(TestFactory.makeTest() instanceof UnitTest);
     });
   });
   
   describe('for accessors', () => {
     it('a getter name can be static, just prefix it with `static`', () => {
       class UnitTest {
-        get testType() { return 'unit'; }
+        static get testType() { return 'unit'; }
       }
       
       assert.equal(UnitTest.testType, 'unit');
@@ -39,7 +39,7 @@ describe('inside a class you can use the `static` keyword', () => {
     it('even a static getter name can be dynamic/computed at runtime', () => {
       const type = 'test' + 'Type';
       class IntegrationTest {
-        get type() { return 'integration'; }
+        static get [type]() { return 'integration'; }
       }
       
       assert.ok('testType' in IntegrationTest);
