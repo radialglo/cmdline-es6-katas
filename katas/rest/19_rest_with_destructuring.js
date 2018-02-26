1547df899ed242b7658dc10406dea9d3f5ff642b
Anthony  Su <antchovee@gmail.com>
2018-02-26 00:02:48 -0800

    Finished Lesson 19: Rest with Destructuring

--- katas/rest/19_rest_with_destructuring.js
+++ katas/rest/19_rest_with_destructuring.js
@@ -5,25 +5,25 @@ let assert = require('chai').assert
 describe('rest with destructuring', () => {
     
   it('rest parameter must be last', () => {
-    const [last, ...all] = [1, 2, 3, 4];
+    const [...all] = [1, 2, 3, 4];
     assert.deepEqual(all, [1, 2, 3, 4]);
   });
   
   it('assign rest of an array to a variable', () => {
-    const [...all] = [1, 2, 3, 4];
+    const [, ...all] = [1, 2, 3, 4];
     assert.deepEqual(all, [2, 3, 4]);
   });
   
   // the following are actually using `spread` ... oops, to be fixed
   it('concat differently', () => {
     const theEnd = [3, 4];
-    const allInOne = [1, 2, ...[theEnd]];
+    const allInOne = [1, 2, ...theEnd];
     assert.deepEqual(allInOne, [1, 2, 3, 4]);
   });
   
   it('`apply` made simple, even for constructors', () => {
     const theDate = [2015, 1, 1];
-    const date = new Date(theDate);
+    const date = new Date(...theDate);
     assert.deepEqual(new Date(2015, 1, 1), date);
   });
   
