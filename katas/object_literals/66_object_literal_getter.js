4935141b6df98693ff7f69a95c24b8e274facd59
Anthony  Su <antchovee@gmail.com>
2018-03-01 01:54:23 -0800

    Finished Lesson 66: Object Literal - getter ``` const obj = {     get x() { return 'ax'; } }; delete obj.x // deletes property / getter ```

--- katas/object_literals/66_object_literal_getter.js
+++ katas/object_literals/66_object_literal_getter.js
@@ -6,7 +6,7 @@ describe('An object literal can also contain getters', () => {
 
   it('just prefix the property with `get` (and make it a function)', function() {
     const obj = {
-      x() { return 'ax'; }
+      get x() { return 'ax'; }
     };
     
     assert.equal(obj.x, 'ax');
@@ -14,7 +14,7 @@ describe('An object literal can also contain getters', () => {
 
   it('must have NO parameters', function() {
     const obj = {
-      x(param) { return 'ax'; }
+      get x() { return 'ax'; }
     };
     
     assert.equal(obj.x, 'ax');
@@ -24,7 +24,7 @@ describe('An object literal can also contain getters', () => {
   it('can be a computed property (an expression enclosed in `[]`)', function() {
     const keyName = 'x';
     const obj = {
-      get keyName() { return 'ax'; }
+      get [keyName]() { return 'ax'; }
     };
     
     assert.equal(obj.x, 'ax');
@@ -34,6 +34,8 @@ describe('An object literal can also contain getters', () => {
     const obj = {
       get x() { return 'ax'; }
     };
+
+    delete obj.x
     
     assert.equal(obj.x, void 0);
   });
