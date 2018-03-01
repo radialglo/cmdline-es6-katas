652a9802f12247b7951cb63712792584b5743851
Anthony  Su <antchovee@gmail.com>
2018-02-28 21:30:53 -0800

    Finished Lesson 47: Set Basics. '`Set` lets you store unique values of any type'

--- katas/set/47_set_basics.js
+++ katas/set/47_set_basics.js
@@ -12,7 +12,7 @@ describe('`Set` lets you store unique values of any type', function(){
     let set = new Set();
     
     set.add(1);
-    set.add(1);
+    set.add(2);
     const expectedSize = 2;
     
     assert.equal(set.size, expectedSize);
@@ -21,6 +21,7 @@ describe('`Set` lets you store unique values of any type', function(){
   it('the string "1" is different to the number 1', function() {
     let set = new Set();
     set.add(1);
+    set.add("1");
 
     assert.equal(set.size, 2);
   });
@@ -28,7 +29,7 @@ describe('`Set` lets you store unique values of any type', function(){
   it('even NaN is equal to NaN', function() {
     let set = new Set();
     set.add(NaN);
-    set.add(Na);
+    set.add(NaN);
     
     assert.equal(set.size, 1);
   });
@@ -36,8 +37,7 @@ describe('`Set` lets you store unique values of any type', function(){
   it('+0 and -0 are seen as equal', function() {
     let set = new Set();
     set.add(+0);
-    set.add(0);
-    set.add('-0');
+    set.add(-0);
     
     assert.deepEqual([...set.values()], [+0]);
   });
